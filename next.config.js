4e96699e8cd5a84f83356ae691e98ff7ba23f162
BlacRubick <213507@ids.upchiapas.edu.mx>
2024-07-06 20:15:42 -0600

    second try

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// next.config.js
 
-module.exports = nextConfig
+const nextConfig = {
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      // Aquí ajustamos la configuración de webpack
+      config.bail = false; // Desactiva el modo bail en webpack
+  
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
